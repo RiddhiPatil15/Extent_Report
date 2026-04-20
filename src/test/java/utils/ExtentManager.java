83d97ae9df5916fe5656c5473892f1d1dc3e02b5
Riddhi <140433388+RiddhiPatil15@users.noreply.github.com>
2026-04-20 10:33:56 +0530

    Update ExtentManager.java

--- src/test/java/utils/ExtentManager.java
+++ src/test/java/utils/ExtentManager.java
@@ -3,13 +3,25 @@ package utils;
 import com.aventstack.extentreports.*;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+import java.io.File;
+
 public class ExtentManager {
 
     private static ExtentReports extent;
 
     public static ExtentReports getInstance() {
         if (extent == null) {
-            ExtentSparkReporter reporter = new ExtentSparkReporter("target/ExtentReport.html");
+
+            // Create folder if it doesn't exist
+            String reportDir = "target/extentreports/";
+            File dir = new File(reportDir);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            // Set report path inside folder
+            ExtentSparkReporter reporter = new ExtentSparkReporter(reportDir + "ExtentReport.html");
+
             extent = new ExtentReports();
             extent.attachReporter(reporter);
         }
